489305f20d93b31f920664c16ca9c280953ff434
adriraj2000 <adrirajc@gmail.com>
2021-02-04 11:57:54 +0530

    basic layout done

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const path=require('path');
+const http=require('http')
+const express=require('express');
+const socketIO=require('socket.io');
+
+
+const port=process.env.PORT||3000;
+const app = express()
+var server = http.createServer(app)
+var io=socketIO(server);
+
+
+io.on('connection',(socket)=>{
+    socket.on('join',(params,callback)=>{
+        
+    })
+
+    socket.on('createMessage',(message,callback)=>{
+
+    })
+
+    socket.on('createLocationMessage',(coords)=>{
+
+    })
+
+    socket.on('typing', (data)=>{
+
+    })
+
+    socket.on('disconnect',()=>{
+
+    })
+})
+
+
+server.listen(port,()=>{
+	console.log(`Server is up on ${port}`);
+})
