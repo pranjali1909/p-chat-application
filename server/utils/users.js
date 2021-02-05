345ac23dc9948ddaf872b1f22aadc1a9aa76b910
adriraj2000 <adrirajc@gmail.com>
2021-02-05 20:06:29 +0530

    will implement users.js

---
+++ server/utils/users.js
@@ -0,0 +1,44 @@
+//
+// ─────────────────────────────────────────────── TO KEEP TRACK OF THE USERS ─────
+//
+
+users = [];
+
+//
+// ─── ADD USER ───────────────────────────────────────────────────────────────────
+//
+
+var addUser = ({ id, username, room }) => {
+
+}
+
+//
+// ─── REMOVE USER BY ID ──────────────────────────────────────────────────────────
+//
+
+var removeUser = (id) => {
+
+}
+
+//
+// ─── GET USER ───────────────────────────────────────────────────────────────────
+//
+
+var getUser = (id) => {
+
+}
+
+//
+// ─── GET USER LIST IN A ROOM ────────────────────────────────────────────────────
+//
+
+var getUserList = (room) => {
+
+}
+
+module.exports = {
+    getUser,
+    getUserList,
+    removeUser,
+    addUser
+}
