113e0c8dc3079ed5a789e0e8d0cf45556b1d26d8
adriraj2000 <adrirajc@gmail.com>
2021-02-06 12:00:12 +0530

    ready for deplloy

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,7 +16,7 @@ function scrollToBottom() {
 
 socket.on('connect', function () {
     // console.log('Connected to server');
-    var params = $.deparam(window.location.search);
+    var params = jQuery.deparam(window.location.search);
     socket.emit('join', params, function (err) {
         if (err) {
             alert(err);
--- server/utils/users.js
+++ server/utils/users.js
@@ -1,76 +1,30 @@
-//
-// ─────────────────────────────────────────────── TO KEEP TRACK OF THE USERS ─────
-//
+class Users {
+	constructor (){
+		this.users=[];
+	}
+	addUser (id,name,room){
+		var user={id,name,room};
+		this.users.push(user);
+		return user ;
+	}
+	removeUser(id){
+		var user=this.getUser(id);
+		if(user){
+			this.users=this.users.filter((user)=>user.id !== id)
+		}
+		return user;
+		// var users=this.users.filter((user)=>user.id !== id);
+		// return users;
 
-users = [];
-
-//
-// ─── ADD USER ───────────────────────────────────────────────────────────────────
-//
-
-var addUser = ({ id, username, room }) => {
-    //Clean the data
-    username = username.trim().toLowerCase();
-    room = room.trim().toLowerCase();
-
-    //Validate the data
-    if (!username || !room) {
-        return {
-            error: "Username and room are required"
-        }
-    }
-
-    //Check for existing user
-    var existingUser = users.find((user) => {
-        return (user.room === room && user.username === username)
-    })
-
-    //Validate Username
-    if (existingUser) {
-        return {
-            error: "Username in use"
-        }
-    }
-
-    //Store user
-    var user = { id, username, room };
-    users.push(user);
-    return { user };
+	}
+	getUser(id){
+		return this.users.filter((user)=>user.id === id)[0];
+	}
+	getUserList(room){
+		var users=this.users.filter((user)=>user.room === room);
+		var nameArray=users.map((user)=>user.name);
+		return nameArray;
+	}
 }
 
-//
-// ─── REMOVE USER BY ID ──────────────────────────────────────────────────────────
-//
-
-var removeUser = (id) => {
-    var index = users.findIndex((user) => {
-        return user.id === id
-    });
-
-    if (index != -1) {
-        return users.splice(index, 1)[0];
-    }
-}
-
-//
-// ─── GET USER ───────────────────────────────────────────────────────────────────
-//
-
-var getUser = (id) => {
-    return users.find((user) => user.id === id)
-}
-
-//
-// ─── GET USER LIST IN A ROOM ────────────────────────────────────────────────────
-//
-
-var getUserList = (room) => {
-    return users.filter((user) => user.room === room);
-}
-
-module.exports = {
-    getUser,
-    getUserList,
-    removeUser,
-    addUser
-}
+module.exports={Users};
